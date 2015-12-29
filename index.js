94aed809fedcdb31d78286a95589c639309570fb
wevanscfi <pevanscfi@gmail.com>
2015-12-29 10:17:52 -0500

    Added options for passing an address or lat and long from the command line

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ var http = require('http'),
     https = require('https'),
     publicIp = require('public-ip'),
     clc = require('cli-color'),
+    parseArgs = require('minimist'),
     Format = require('./lib/Format');
 
+var ip = '',
+    location_options = {},
+    location = '',
+    lat,
+    long;
+
 // gets public ip address
-
-var ip = '';
-
 publicIp(function (err, res) {
 
     if (err) {
@@ -26,37 +30,70 @@ publicIp(function (err, res) {
     }
 });
 
-var location_options = {
+
+
+var argv = parseArgs(process.argv.slice(2), opts={});
+
+if (argv.address) {
+    console.log(clc.green('✓ address: ') + clc.bgBlack.white(argv.address));
+    location_options = {
+        host: 'maps.googleapis.com',
+        path: '/maps/api/geocode/json?address=' + encodeURIComponent(argv.address.toString()),
+        method: 'GET'
+    };
+    http.get(location_options, function (res) {
+        var text = '';
+        res
+            .on('data', function (chunk) {
+                text +=  chunk.toString();
+            })
+            .on('end', function () {
+                var json = JSON.parse(text);
+                console.log(clc.green('✓ got geo location server response'));
+
+                lat = json.results[0].geometry.location.lat;
+                long = json.results[0].geometry.location.lng;
+
+                // sends HTTP request to weather server
+                weatherRequest();
+            })
+    });
+
+} else if (argv.l && argv.o) {
+    lat = argv.l;
+    long = argv.o;
+    console.log(clc.green('✓ manual lat long set'));
+
+    // sends HTTP request to weather server
+    weatherRequest();
+
+} else {
+    location_options = {
         host: 'freegeoip.net',
         path: '/json/' + ip,
         method: 'GET'
     };
 
-// gets location based on retrieved public ip
+    http.get(location_options, function (res) {
+        var text = '';
+        res
+            .on('data', function (chunk) {
+                console.log(clc.green('✓ got location server response'));
+                text +=  chunk.toString();
+            })
+            .on('end', function () {
+                var json = JSON.parse(text);
+                location = json.city + ', ' + json.region_name + ', ' + json.country_name;
+                lat = json.latitude;
+                long = json.longitude;
 
-var location = '',
-    lat,
-    long;
+                console.log(clc.green('✓ got location: ') + clc.bgBlack.white(location));
 
-http.get(location_options, function (res) {
-    var text = '';
-    res
-        .on('data', function (chunk) {
-            console.log(clc.green('✓ got location server response'));
-            text +=  chunk.toString();
-        })
-        .on('end', function () {
-            var json = JSON.parse(text);
-            location = json.city + ', ' + json.region_name + ', ' + json.country_name;
-            lat = json.latitude;
-            long = json.longitude;
-
-            console.log(clc.green('✓ got location: ') + clc.bgBlack.white(location));
-
-            // sends HTTP request to weather server
-            weatherRequest();
-        })
-});
+                // sends HTTP request to weather server
+                weatherRequest();
+            })
+    });
+}
 
 function weatherRequest() {
     var weather_options = {
