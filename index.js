494b77d119e0b1cd82aea70f625fbfb894af8d5b
apizzimenti <apizzimenti@uiowa.edu>
2015-12-29 12:36:56 -0500

    remove cumbersome stuff

--- index.js
+++ index.js
@@ -6,16 +6,13 @@
 
 // dependencies
 
-var http = require('http'),
-    https = require('https'),
-    publicIp = require('public-ip'),
+var publicIp = require('public-ip'),
     clc = require('cli-color'),
     parseArgs = require('minimist'),
-    Format = require('./lib/Format');
+    Format = require('./lib/Format'),
+    args = require('./lib/args');
 
 var ip = '',
-    location_options = {},
-    location = '',
     lat,
     long;
 
@@ -30,90 +27,17 @@ publicIp(function (err, res) {
     }
 });
 
-
-
 var argv = parseArgs(process.argv.slice(2), opts={});
 
 if (argv.address) {
-    location_options = {
-        host: 'maps.googleapis.com',
-        path: '/maps/api/geocode/json?address=' + encodeURIComponent(argv.address.toString()),
-        method: 'GET'
-    };
-    http.get(location_options, function (res) {
-        var text = '';
-        res
-            .on('data', function (chunk) {
-                text +=  chunk.toString();
-            })
-            .on('end', function () {
-                var json = JSON.parse(text);
-                if (json.status == "OK" && json.results[0]) {
-                    console.log(clc.green('✓ got geo location server response'));
-                    console.log(clc.green('✓ got location: ') + clc.bgBlack.white(json.results[0].formatted_address));
-                    lat = json.results[0].geometry.location.lat;
-                    long = json.results[0].geometry.location.lng;
-                    // sends HTTP request to weather server
-                    weatherRequest();
-                } else {
-                    console.log(clc.red('✗ address not found'));
-                }
-            })
-    });
-
+    args.address(argv.address)
 } else if (argv.lat && argv.long) {
     lat = argv.lat;
     long = argv.long;
     console.log(clc.green('✓ manual lat long set'));
 
     // sends HTTP request to weather server
-    weatherRequest();
-
+    args.weatherRequest();
 } else {
-    location_options = {
-        host: 'freegeoip.net',
-        path: '/json/' + ip,
-        method: 'GET'
-    };
-
-    http.get(location_options, function (res) {
-        var text = '';
-        res
-            .on('data', function (chunk) {
-                console.log(clc.green('✓ got location server response'));
-                text +=  chunk.toString();
-            })
-            .on('end', function () {
-                var json = JSON.parse(text);
-                location = json.city + ', ' + json.region_name + ', ' + json.country_name;
-                lat = json.latitude;
-                long = json.longitude;
-
-                console.log(clc.green('✓ got location: ') + clc.bgBlack.white(location));
-
-                // sends HTTP request to weather server
-                weatherRequest();
-            })
-    });
-}
-
-function weatherRequest() {
-    var weather_options = {
-            host: 'api.forecast.io',
-            path: '/forecast/d399f7331297381cd6b95106add0d22d/' + lat.toString() + ',' + long.toString(),
-            method: 'GET'
-        };
-
-    https.get(weather_options, function(res) {
-        var json  = '';
-        res
-            .on('data', function (chunk) {
-                json += chunk;
-            })
-            .on('end', function() {
-                console.log(clc.green('✓ got data from weather server'));
-                var weather = Format(json);
-                console.log(weather.weather);
-            })
-    });
+    args.automatic(ip);
 }
