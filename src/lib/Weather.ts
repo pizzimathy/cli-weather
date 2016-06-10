454990e038950290f8792db62bdb6fd0778247f3
apizzimenti <pizzimentianthony@gmail.com>
2016-06-10 14:25:32 -0500

    assign fields

--- src/lib/Weather.ts
+++ src/lib/Weather.ts
@@ -4,3 +4,27 @@
 
 import axios = require("axios");
 
+export class Weather {
+    current: {
+        real_temp: string,
+        feels_temp: string,
+        wind: string,
+        conditions: string
+    };
+
+    forecast: Day[];
+
+    constructor (lat: string, long: string) {
+    }
+}
+
+class Day {
+    date: string;
+    high: string;
+    low: string;
+    precipitation: string;
+    summary: string;
+    icon: string;
+    sunrise: string;
+    sunset: string;
+}
